b49c6d01e43e1cb9eb1c8e7868990040d2f8aa52
Anderson Rancan <anderson.rancan@gmail.com>
2017-12-29 17:38:55 +0100

    basic ASP.NET MVC project

---
+++ source/CrossBeacon.WebMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrossBeacon.WebMvc.Startup))]
+namespace CrossBeacon.WebMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
